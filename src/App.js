64402c2eba962a865bb20adbee55813c15ddc384
貟林杰 <yun.linjie@eland.co.kr>
2017-03-16 17:42:10 +0800

    modify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'antd-mobile';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        111111
+        <Button className="btn" type="primary">primary 按钮</Button>
       </div>
     );
   }
