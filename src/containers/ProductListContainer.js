c4a5b3ec77270d41c5c909a823ee68dfcbca2121
李澈 <281342000@qq.com>
2017-04-10 15:49:19 +0800

    test

--- src/containers/ProductListContainer.js
+++ src/containers/ProductListContainer.js
@@ -159,6 +159,7 @@ export default class ProductListContainer extends Component {
     )
     scanQRCode = () => {
         let self = this;
+        alert("scanQRCode")
         wx.scanQRCode({
             needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
             scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
