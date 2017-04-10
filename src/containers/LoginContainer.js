6c7d44320402ed150aec085d42f1f95311393e07
李澈 <281342000@qq.com>
2017-04-10 13:41:46 +0800

    add account token

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -17,6 +17,7 @@ class Login extends Component {
     componentDidMount() {
         let token = getQueryString("token");
         if(!token){
+            console.lon("account token ===>",token)
             this.setState({animating:false})
             Toast.fail('登陆失败', 3);
             return;
