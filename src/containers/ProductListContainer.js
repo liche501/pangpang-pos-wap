e8b5ab1b52ad24bbe53d4ce1d8165fcfacb5feb5
李澈 <281342000@qq.com>
2017-04-10 15:50:43 +0800

    test

--- src/containers/ProductListContainer.js
+++ src/containers/ProductListContainer.js
@@ -165,7 +165,8 @@ export default class ProductListContainer extends Component {
             scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
             success: function (res) {
                 var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-                self.getSku("42AB616-6")
+                alert(result)
+                // self.getSku("42AB616-6")
                 // alert(result);
             }
         })
