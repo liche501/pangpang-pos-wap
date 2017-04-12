1999d5435aafe00ddf68d81b4d417371420a3631
貟林杰 <yun.linjie@eland.co.kr>
2017-04-12 17:11:26 +0800

    页面样式更改

--- src/containers/PayContainer.js
+++ src/containers/PayContainer.js
@@ -10,9 +10,9 @@ import pay2 from '../../public/wxzf.gif';
 const isIPhone = new RegExp('\\biPhone\\b|\\biPod\\b', 'i').test(window.navigator.userAgent);
 let wrapProps;
 if (isIPhone) {
-  wrapProps = {
-    onTouchStart: e => e.preventDefault(),
-  };
+    wrapProps = {
+        onTouchStart: e => e.preventDefault(),
+    };
 }
 
 const Item = List.Item;
@@ -23,24 +23,24 @@ const styles = {};
 const menuName = "支付中心"
 
 const formatPayMethod = (type) => {
-    switch(type){
+    switch (type) {
         case "mileage":
-                return "积分"
+            return "积分"
             break;
-            case "ali":
-                return "支付宝"
+        case "ali":
+            return "支付宝"
             break;
-            case "weixin":
-                return "微信"
+        case "weixin":
+            return "微信"
             break;
-            case "card":
-                return "刷卡"
+        case "card":
+            return "刷卡"
             break;
-            case "cash":
-                return "现金"
+        case "cash":
+            return "现金"
             break;
-            default:
-                return "title"
+        default:
+            return "title"
             break;
     }
 }
@@ -48,12 +48,12 @@ const formatPayMethod = (type) => {
 export default class PayContainer extends Component {
     state = {
         menuName: "结算中心",
-        completePay:false,
-        salePrice:0,
-        remainAmount:0,
-        payments:[],
+        completePay: false,
+        salePrice: 0,
+        remainAmount: 0,
+        payments: [],
     }
-    
+
     componentWillMount() {
         this.refreshCartData()
     }
@@ -62,7 +62,7 @@ export default class PayContainer extends Component {
 
         if (cartId) {
             cartAPI.getCartById(cartId).then(res => {
-                console.log('refreshCartData====>',res.result)
+                console.log('refreshCartData====>', res.result)
                 if (res.success) {
                     let rs = res.result
                     this.setState({
@@ -101,70 +101,65 @@ export default class PayContainer extends Component {
         });
     }
     _inputButtonClick = (type) => {
-        switch(type){
+        switch (type) {
             case "mileage":
-                this.showPrompt("积分",type)
-            break;
+                this.showPrompt("积分", type)
+                break;
             case "ali":
-                this.showPrompt("支付宝",type)
-            break;
+                this.showPrompt("支付宝", type)
+                break;
             case "weixin":
-                this.showPrompt("微信",type)
-            break;
+                this.showPrompt("微信", type)
+                break;
             case "card":
-                this.showPrompt("刷卡",type)
-            break;
+                this.showPrompt("刷卡", type)
+                break;
             case "cash":
-                this.showPrompt("现金",type)
-            break;
+                this.showPrompt("现金", type)
+                break;
             default:
-            break;
+                break;
         }
-        
+
     }
-    showPrompt = (title,type) => {
+    showPrompt = (title, type) => {
         let cartId = sessionStorage.getItem("cartId")
         prompt(
-                title, '请输入金额',
-                [
-                    { text: '取消' },
-                    {
-                        text: '保存',
-                        onPress: (value) => {
-                            if(!value){
-                                Toast.info('金额为空', 1);
-                                return
-                            }
-                            if(type === "mileage"){
-                                cartAPI.setPaymentForMileage(cartId,{"amount":parseFloat(value)}).then(res => {
-                                    this.refreshCartData();
-                                })
-                            }else{
-                                cartAPI.setPayment(cartId,{"method":type,"amount":parseFloat(value)}).then(res => {
-                                    this.refreshCartData();
-                                })
-                            }
+            title, '请输入金额',
+            [
+                { text: '取消' },
+                {
+                    text: '保存',
+                    onPress: (value) => {
+                        if (!value) {
+                            Toast.info('金额为空', 1);
+                            return
+                        }
+                        if (type === "mileage") {
+                            cartAPI.setPaymentForMileage(cartId, { "amount": parseFloat(value) }).then(res => {
+                                this.refreshCartData();
+                            })
+                        } else {
+                            cartAPI.setPayment(cartId, { "method": type, "amount": parseFloat(value) }).then(res => {
+                                this.refreshCartData();
+                            })
                         }
                     }
-                ]
-            )
+                }
+            ]
+        )
     }
     render() {
         return (
             <div style={{ background: '#f6f6f6' }}>
-                <Navi leftIcon="left" 
-                      title={menuName} 
-                      onLeftClick={() => { history.back() } } 
-                />
-                <List>
-                    <div style={styles.total}>
-                        <span style={{  }}>已支付：￥{this.state.salePrice-this.state.remainAmount}元</span>
-                        <span style={{ float: 'right' }}>未支付：￥{this.state.remainAmount}元</span>
-                    </div>
-                </List>
+                <Navi leftIcon="left"
+                    title={menuName}
+                    onLeftClick={() => { history.back() } }
+                    />
+                <p style={{marginLeft:'0.5rem',fontSize:'0.35rem'}}>选择支付方式</p>
                 <List>
                     <div style={styles.pay}>
-                        <RadioItem className='pay-am-list-item-middle' key='Alipay' checked={false} onClick={()=>this._inputButtonClick("ali")} >
+                        <RadioItem className='pay-am-list-item-middle' key='Alipay' checked={false} onClick={() => this._inputButtonClick("ali")} >
                             <div style={{ textAlign: 'center' }}>
                                 <img src={pay1} style={{ marginRight: '5px' }} alt="" /> &nbsp;
                                     <span>支付宝</span>
@@ -172,7 +167,7 @@ export default class PayContainer extends Component {
                         </RadioItem>
                     </div>
                     <div style={styles.paywx}>
-                        <RadioItem className='pay-am-list-item-middle' key='Wxpay' checked={false} onClick={()=>this._inputButtonClick("weixin")}>
+                        <RadioItem className='pay-am-list-item-middle' key='Wxpay' checked={false} onClick={() => this._inputButtonClick("weixin")}>
                             <div style={{ textAlign: 'center' }}>
                                 <img src={pay2} style={styles.img1} alt="" /> &nbsp;
                                 <span>微&nbsp;信</span>
@@ -180,9 +175,10 @@ export default class PayContainer extends Component {
                         </RadioItem>
                     </div>
                 </List>
+                <WhiteSpace />
                 <List>
                     <div style={styles.pay}>
-                        <RadioItem className='pay-am-list-item-middle' key='card' checked={false} onClick={()=>this._inputButtonClick("card")}>
+                        <RadioItem className='pay-am-list-item-middle' key='card' checked={false} onClick={() => this._inputButtonClick("card")}>
                             <div style={{ textAlign: 'center' }}>
                                 <img src={pay1} style={{ marginRight: '5px' }} alt="" /> &nbsp;
                                     <span>&nbsp;刷&nbsp;卡&nbsp;&nbsp;</span>
@@ -190,7 +186,7 @@ export default class PayContainer extends Component {
                         </RadioItem>
                     </div>
                     <div style={styles.paywx}>
-                        <RadioItem className='pay-am-list-item-middle' key='cash' checked={false} onClick={()=>this._inputButtonClick("cash")}>
+                        <RadioItem className='pay-am-list-item-middle' key='cash' checked={false} onClick={() => this._inputButtonClick("cash")}>
                             <div style={{ textAlign: 'center' }} >
                                 <img src={pay2} style={styles.img1} alt="" /> &nbsp;
                                 <span>现&nbsp;金</span>
@@ -198,7 +194,20 @@ export default class PayContainer extends Component {
                         </RadioItem>
                     </div>
                 </List>
-                <Button className="btn" type="primary" style={styles.btn} onClick={() => this._placeOrderClick()}>支付完成</Button>
+                <WhiteSpace />
+                <List>
+                    <div style={styles.total}>
+                        <p>
+                            已支付：
+                            <span style={{ float: 'right' }}>￥{this.state.salePrice - this.state.remainAmount}元</span>
+                        </p>
+                        <p>
+                            未支付：
+                            <span style={{ float: 'right' }}>￥{this.state.remainAmount}元</span>
+                        </p>
+                    </div>
+                </List>
+                <Button className="btn" type="primary" style={styles.btn} onClick={() => this._placeOrderClick()}>确认支付￥{this.state.salePrice}元</Button>
             </div>
         )
     }
@@ -208,14 +217,14 @@ styles = {
     pay: {
         width: '50%',
         // width: '3.5rem',
-        borderRight: '5px solid #ddd' 
+        borderRight: '0.15rem solid #F6F6F6'
     },
     paywx: {
-        width: '45%',
+        width: '43%',
         // width: '3.3rem',
-        position:'absolute',
-        top:'0',
-        right:'30px' 
+        position: 'absolute',
+        top: '0',
+        right: '30px'
     },
     img1: {
         width: '50px',
@@ -226,14 +235,14 @@ styles = {
         // width: '5.5rem',
         fontSize: '0.35rem',
         margin: '0 auto',
-        paddingBottom: '1px' 
+        paddingBottom: '1px'
     },
     total: {
         width: '75%',
         // width: '5.5rem',
         fontSize: '0.35rem',
         margin: '35px auto 0',
-        paddingBottom: '35px' 
+        paddingBottom: '1px'
     },
     btn: {
         width: '90%',
--- src/containers/SettlementContainer.js
+++ src/containers/SettlementContainer.js
@@ -34,7 +34,7 @@ export default class SettlementContainer extends Component {
         availableMileage: 0,
         isSetCustomer: false,
         isSetCoupon: false,
-        
+
     }
 
     componentWillMount() {
@@ -50,7 +50,7 @@ export default class SettlementContainer extends Component {
 
         if (cartId) {
             cartAPI.getCartById(cartId).then(res => {
-                console.log('====>',res.result)
+                console.log('====>', res.result)
                 if (res.success) {
                     if (res.result.items !== null) {
                         this.setState({
@@ -61,9 +61,9 @@ export default class SettlementContainer extends Component {
                     } else {
                         this.setState({ salePrice: 0, discount: 0 });
                     }
-                    this.setState({isSetCoupon:res.result.couponNo ? true : false});
-                    this.setState({couponNo:res.result.couponNo});
-                    this.setState({availableMileage:res.result.availableMileage});
+                    this.setState({ isSetCoupon: res.result.couponNo ? true : false });
+                    this.setState({ couponNo: res.result.couponNo });
+                    this.setState({ availableMileage: res.result.availableMileage });
                     if (res.result.customerInfo !== null) {
                         this.setState({
                             customerId: res.result.customerInfo.id,
@@ -78,7 +78,7 @@ export default class SettlementContainer extends Component {
                     else {
                         this.setState({
                             customerId: 0,
-                            customerMobile:"",
+                            customerMobile: "",
                             customerNo: '',
                             customerGrade: 0
                         });
@@ -101,7 +101,7 @@ export default class SettlementContainer extends Component {
                     {
                         text: '保存',
                         onPress: (value) => {
-                            if(!value){
+                            if (!value) {
                                 Toast.info('顾客号为空', 1);
                                 return
                             }
@@ -119,7 +119,7 @@ export default class SettlementContainer extends Component {
                     {
                         text: '保存',
                         onPress: (value) => {
-                            if(!value){
+                            if (!value) {
                                 Toast.info('优惠卷为空', 1);
                                 return
                             }
@@ -138,7 +138,7 @@ export default class SettlementContainer extends Component {
         //     success: function (res) {
         //         var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
         //         alert(result);
-                
+
         //     }
         // })
 
@@ -147,49 +147,51 @@ export default class SettlementContainer extends Component {
         } else if (type === 'coupon') {
             // alert("coupon")
             this.setCoupon("EE4EEDE03E762A2AA2")
-        }   
+        }
     }
-    setCustomer = (value) =>{
+    setCustomer = (value) => {
         cartAPI.setCustomer(sessionStorage.getItem('cartId'), { "no": value })
-        .then(res => {
-            this.setState({isSetCustomer:true});
-            Toast.info('会员保存成功', 1);
-            this.refreshCartData();
-        })
-        .catch(error => {
-            Toast.fail('会员保存失败', 1);
-        })
+            .then(res => {
+                this.setState({ isSetCustomer: true });
+                Toast.info('会员保存成功', 1);
+                this.refreshCartData();
+            })
+            .catch(error => {
+                Toast.fail('会员保存失败', 1);
+            })
     }
     setCoupon = (value) => {
         cartAPI.setCoupon(sessionStorage.getItem('cartId'), { "no": value })
-        .then(res => {
-            console.log(res)
-            this.setState({isSetCoupon:true})
-            Toast.info('优惠券保存成功', 1);
-            this.refreshCartData();
-        })
-        .catch(error => {
-            Toast.fail('优惠券保存失败', 1);
-        })
+            .then(res => {
+                console.log(res)
+                this.setState({ isSetCoupon: true })
+                Toast.info('优惠券保存成功', 1);
+                this.refreshCartData();
+            })
+            .catch(error => {
+                Toast.fail('优惠券保存失败', 1);
+            })
     }
     _cancelCart = () => {
         let cartId = sessionStorage.getItem("cartId");
         antAlert('取消订单', '确定取消订单么?', [
             { text: '取消', onPress: () => console.log('cancel') },
-            { text: '确定', onPress: () => {
-                cartAPI.removeCartById(cartId).then(res => {
-                    if(res.success){
-                        sessionStorage.removeItem("cartId")
-                        window.location.href = "/"
-                    }
-                })
-            }, style: { fontWeight: 'bold' } },
+            {
+                text: '确定', onPress: () => {
+                    cartAPI.removeCartById(cartId).then(res => {
+                        if (res.success) {
+                            sessionStorage.removeItem("cartId")
+                            window.location.href = "/"
+                        }
+                    })
+                }, style: { fontWeight: 'bold' }
+            },
         ])
     }
     render() {
-        let customerContent,couponContent;
+        let customerContent, couponContent;
 
-        if(this.state.isSetCustomer){
+        if (this.state.isSetCustomer) {
             customerContent = (
                 <div className="row"  >
                     <div style={styles.div}>
@@ -200,32 +202,35 @@ export default class SettlementContainer extends Component {
                             <div>手机号: {this.state.customerMobile}</div>
                             <div>会员号 : {this.state.customerNo}</div>
                             <div>
-                                积&nbsp;&nbsp;&nbsp;分 : 
+                                积&nbsp;&nbsp;&nbsp;分 :
                                 <span style={{ color: 'orange' }}>{this.state.currentPoints}</span>
                             </div>
                             <div>
-                                本次可用积分 : 
+                                本次可用积分 :
                                 <span style={{ color: 'orange' }}>{this.state.availableMileage}</span>
                             </div>
                         </div>
                     </div>
                 </div>
             )
-        }else{
+        } else {
             customerContent = (
                 <List>
                     <Item data-seed="logId" style={styles.background}>
                         <div style={styles.div3}>
-                            <MdFullscreen style={styles.span} onClick={() => { this._scanButtonClick("customer") } }></MdFullscreen>
-                            <span>|</span>
-                            <Button onClick={() => this._inputButtonClick('customer')} size='small' style={styles.btn1}>Customer</Button>
+                            <div>
+                                <Button onClick={() => this._inputButtonClick('customer')} size='small' style={styles.btn1}>会员登录</Button>
+                            </div>
+                            <div style={{ position: 'absolute', top: 0, left: '4rem' }}>
+                                <MdFullscreen style={styles.span} onClick={() => { this._scanButtonClick("customer") } }></MdFullscreen>
+                            </div>
                         </div>
                     </Item>
                 </List>
             )
         }
 
-        if(this.state.isSetCoupon){
+        if (this.state.isSetCoupon) {
             couponContent = (
                 <WingBlank>
                     <List >
@@ -235,7 +240,7 @@ export default class SettlementContainer extends Component {
                                     优惠券
                                 </div>
                                 <div style={styles.coupon}>
-                                    <span style={{fontSize:"0.25rem"}}>{this.state.couponNo}</span>
+                                    <span style={{ fontSize: "0.25rem" }}>{this.state.couponNo}</span>
                                     <div style={styles.date}>2017.05.01~2017.06.01</div>
                                 </div>
                             </div>
@@ -243,31 +248,35 @@ export default class SettlementContainer extends Component {
                     </List>
                 </WingBlank>
             )
-        }else{
+        } else {
             couponContent = (
                 <List>
                     <Item data-seed="logId" style={styles.background}>
                         <div style={styles.div3}>
-                            <MdFullscreen style={styles.span} onClick={() => { this._scanButtonClick("coupon") } }></MdFullscreen>
-                            <span>|</span>
-                            <Button onClick={() => this._inputButtonClick('coupon')} size='small' style={styles.btn2}>Coupon/SALE</Button>
+                            <div>
+                                <Button onClick={() => this._inputButtonClick('coupon')} size='small' style={styles.btn1}>查询优惠券</Button>
+                            </div>
+                            <div style={{ position: 'absolute', top: 0, left: '4rem' }}>
+                                <MdFullscreen style={styles.span} onClick={() => { this._scanButtonClick("coupon") } }></MdFullscreen>
+                            </div>
                         </div>
                     </Item>
                 </List>
-                
+
             )
         }
 
 
         return (
             <div style={{ background: '#f6f6f6' }}>
-                <Navi leftIcon="left" 
-                      title={this.state.menuName} 
-                      onLeftClick={() => { history.back() } } 
-                      rightIcon="text" 
-                      rightText="取消订单" 
-                      onRightClick={this._cancelCart}
-                />
+                <Navi leftIcon="left"
+                    title={this.state.menuName}
+                    onLeftClick={() => { history.back() } }
+                    rightIcon="text"
+                    rightText="取消订单"
+                    onRightClick={this._cancelCart}
+                    />
+                <WhiteSpace />
                 {couponContent}
                 <WhiteSpace />
                 {customerContent}
@@ -282,6 +291,10 @@ export default class SettlementContainer extends Component {
                             优惠：
                             <span style={{ float: 'right' }}>-￥{this.state.discount}元</span>
                         </p>
+                        <p>
+                            积分：
+                            <span style={{ float: 'right' }}>-￥{this.state.discount}元</span>
+                        </p>
                     </div>
                 </List>
                 <List>
@@ -292,7 +305,7 @@ export default class SettlementContainer extends Component {
                 </List>
                 <WhiteSpace />
 
-                <Button className="btn" type="primary" style={styles.btn} onClick={() => window.location.href = `/#/paylist?availableMileage=${this.state.availableMileage}`}>去支付</Button>
+                <Button className="btn" type="primary" style={styles.btn} onClick={() => window.location.href = `/#/paylist?availableMileage=${this.state.availableMileage}`}>提交订单</Button>
             </div>
         )
     }
@@ -316,10 +329,10 @@ styles = {
     },
     input: {
         fontSize: '0.25rem',
-        textAlign:'center',
+        textAlign: 'center',
         borderRadius: '10px',
         width: '1.85rem',
-        height: '0.6rem' 
+        height: '0.6rem'
     },
     // div2: {
     //     marginBottom: '0.16rem',
@@ -331,10 +344,11 @@ styles = {
         color: '#42A2EA',
         fontWeight: 'bold',
         // marginLeft: '0.2rem',
-        borderRight: '1px solid #e8e8e8' 
+        borderRight: '1px solid #e8e8e8'
     },
     div3: {
-        lineHeight: '2',
+        // lineHeight: '2',
+        lineHeight: '3.5',
         textAlign: 'left'
     },
     div4: {
@@ -344,38 +358,38 @@ styles = {
     coupon: {
         position: 'absolute',
         // top: '0.05rem',
-        top:0,
-        left: '2.8rem' 
+        top: 0,
+        left: '2.8rem'
     },
-    date:{
+    date: {
         fontSize: '0.2rem',
-        marginTop: '-0.15rem' 
+        marginTop: '-0.15rem'
     },
     info: {
         width: '75%',
         // width: '5.5rem',
         fontSize: '0.35rem',
         margin: '0 auto',
-        paddingBottom: '1px' 
+        paddingBottom: '1px'
     },
     total: {
         width: '75%',
         // width: '5.5rem',
         fontSize: '0.35rem',
         margin: '35px auto 0',
-        paddingBottom: '35px' 
+        paddingBottom: '35px'
     },
     pay: {
         width: '50%',
         // width: '3.5rem',
-        borderRight: '5px solid #ddd' 
+        borderRight: '5px solid #ddd'
     },
     paywx: {
         width: '45%',
         // width: '3.3rem',
-        position:'absolute',
-        top:'0',
-        right:'30px' 
+        position: 'absolute',
+        top: '0',
+        right: '30px'
     },
     p: {
         width: '1rem',
@@ -383,9 +397,11 @@ styles = {
     },
     span: {
         color: '#42A2EA',
-        fontSize: '1rem',
-        marginLeft: '1rem',
-        marginRight: '0.8rem'
+        fontSize: '1.5rem',
+        marginLeft: '0.5rem',
+        marginRight: '0.8rem',
+        paddingLeft:'0.6rem',
+        borderLeft: '1px solid #ececec',
     },
     img: {
         width: '1rem',
@@ -417,14 +433,10 @@ styles = {
         // margin: '0.5rem auto 0',
         fontWeight: 'bold'
     },
-    btn1:{
-        marginLeft: '0.5rem',
+    btn1: {
+        fontSize:'0.35rem',
+        marginLeft: '1rem',
         display: 'inline-block',
-        border: 0 
+        border: 0
     },
-    btn2: {
-        marginLeft: '0.5rem',
-        display: 'inline-block',
-        border: 0 
-    }
 }
